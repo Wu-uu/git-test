e71aa57b8ff24c032c7affe0c96e6745b0fac83d
Nibux <1635949406@qq.com>
2022-12-20 16:52:00 +0800

    first commit

---
+++ src/main/java/com/atwu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atwu;
+
+/**
+ * @Date: 2022/12/20 16:51
+ * @Author: Wu
+ * @Description:
+ */
+public class GitTest {
+   public static void main(String[] args) {
+      System.out.println("hello git!");
+   }
+}
